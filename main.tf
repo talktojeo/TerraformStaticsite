49ac34d2b9fbbf05373b5a7bdb47115726a1d2c4
talktojeo <talktojeo@gmail.com>
2021-09-16 10:08:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,13 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
+  subscription_id = "74e06804-1e02-43fc-83f2-c2ce23048ad0"
   features {}
 }
+
 resource "azurerm_resource_group" "terr_resgroup1" {
   name = "res_group758567"
   location = "uksouth"
-} 
+}
