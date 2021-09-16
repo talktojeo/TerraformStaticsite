68cbb6946eefaf1fcf1a2f75d6e1f7b79abdeefa
talktojeo <talktojeo@gmail.com>
2021-09-16 09:39:39 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.76.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "terr_resgroup1" {
+  name = "res_group758567"
+  location = "uksouth"
+}
