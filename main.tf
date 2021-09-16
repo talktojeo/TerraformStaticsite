bcd7b1229073898a5ebbfcc11b760e439d7ae6c7
talktojeo <talktojeo@gmail.com>
2021-09-16 11:51:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,17 @@
 terraform {
-
-  required_version = ">=0.12"
-  
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
     }
   }
 }
-
 provider "azurerm" {
   features {}
 }
-
 resource "azurerm_resource_group" "rg" {
-  name = "res_grpp2442"
+  name = "res_grpp24542"
   location = "uksouth"
 }
+
+
