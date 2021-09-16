7c8bc2456171849b00258c4100c5f244bbc02d90
josephjeethu <joseph@MindLogicsLtd.onmicrosoft.com>
2021-09-16 15:47:52 +0100

    all version updated

--- main.tf
+++ main.tf
@@ -2,7 +2,31 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "rg" {
-  name     = "resgrpp240042"
+  name     = "resgrp_web_demo19"
   location = "uksouth"
 }
+
+resource "azurerm_app_service" "terr_appsvc" {
+  name                = "azdemoappsrvc"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.terr_appservplan.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+}
+resource "azurerm_app_service_plan" "terr_appservplan" {
+  name                = "app-srv-plan1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+
 
