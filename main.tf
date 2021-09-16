a5f4b42fbdec093072980ac6b0c5198a8fee9574
josephjeethu <joseph@MindLogicsLtd.onmicrosoft.com>
2021-09-16 16:02:20 +0100

    udpated main.tf

--- main.tf
+++ main.tf
@@ -29,9 +29,9 @@ resource "azurerm_app_service_plan" "terr_appservplan" {
 }
 
 resource "azurerm_static_site" "terr_staticsite" {
-  name = "demostttsite446"
+  name                = "demostttsite446"
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
 }
 
 
