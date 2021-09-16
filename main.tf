836e0437e535306fc11781b4b1d82f074b76f4a1
talktojeo <talktojeo@gmail.com>
2021-09-16 10:18:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 terraform {
+
+  required_version = ">=0.12"
+  
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "2.76.0"
+      version = "~>2.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "74e06804-1e02-43fc-83f2-c2ce23048ad0"
-  tenant_id = "14a4d975-55fd-4d44-89f7-c826ac68f929"
 }
 
-resource "azurerm_resource_group" "terr_resgroup1" {
-  name = "res_group758567"
+resource "azurerm_resource_group" "rg" {
+  name = "res_grpp2442"
   location = "uksouth"
 }
