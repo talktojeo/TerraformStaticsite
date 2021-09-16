3e74ca330cddb13d17e928562005017ca24df495
josephjeethu <joseph@MindLogicsLtd.onmicrosoft.com>
2021-09-16 16:15:32 +0100

    changed main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "azurerm" {
   features {}
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "res_terraf_statemanage"
+    storage_account_name = "terrastatemangem99"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
 resource "azurerm_resource_group" "rg" {
   name     = "resgrp_web_demo19"
   location = "uksouth"
