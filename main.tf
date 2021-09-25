4b1a8ccbed0030ec849376253ec04d1b5344d179
talktojeo <talktojeo@gmail.com>
2021-09-25 14:12:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
 provider "azurerm" {
   features {}
 }
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "res_terraf_statemanage"
-    storage_account_name = "terrastatemangem99"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
+
 resource "azurerm_resource_group" "rg" {
   name     = "resgrp_web_demo19"
   location = "westus2"
